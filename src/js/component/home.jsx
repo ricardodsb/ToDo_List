09043d97b274a5de3c2efe1662da178b36d811a7
ricardodsb <ricardojdasilva15@gmail.com>
2021-10-21 19:52:31 +0000

    changes

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,12 +1,28 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 //create your first component
 const Home = () => {
+	const [task, setTask] = useState("");
+	const [list, setList] = useState([""]);
+
 	return (
 		<div className="text-center mt-5">
 			<h1 className="title">{"todo's"}</h1>
+
+			<input
+				id="task"
+				type="text"
+				task="task"
+				placeholder="Add your task here:"
+				onChange={e => {
+					setTask(e.target.value);
+				}}
+				value={task}
+			/>
+			{list.map((item, index) => {
+				return <p key={index}>{item}</p>;
+			})}
 		</div>
 	);
 };
-
 export default Home;
