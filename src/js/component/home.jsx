7ad9bdb0aaf14cea1f1386f5415e9728d702a3c7
ricardodsb <ricardojdasilva15@gmail.com>
2021-10-22 22:21:09 +0000

    some progress

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,28 +1,66 @@
-import React, { useEffect, useState } from "react";
+import React, { useState } from "react";
 
-//create your first component
-const Home = () => {
-	const [task, setTask] = useState("");
-	const [list, setList] = useState([""]);
+const Todo = () => {
+	const [inputTask, setInputTask] = useState("");
+	const [inputList, setInputList] = useState([]);
 
+	function delItem(id) {
+		let setInput = inputList.filter(inputTask => inputTask.id !== id);
+		setInputList(setInput);
+	}
+	function addItem(e) {
+		if (e.keyCode == 13) {
+			setInputTask(inputTask);
+			const list = inputList.concat(inputTask);
+			setInputList(list);
+		}
+	}
 	return (
 		<div className="text-center mt-5">
-			<h1 className="title">{"todo's"}</h1>
-
+			<h1>{"ToDo's"}</h1>
 			<input
-				id="task"
 				type="text"
-				task="task"
-				placeholder="Add your task here:"
-				onChange={e => {
-					setTask(e.target.value);
-				}}
-				value={task}
+				value={inputTask}
+				placeholder="What needs to be done?"
+				onChange={e => setInputTask(e.target.value)}
+				onKeyUp={addItem}
 			/>
-			{list.map((item, index) => {
-				return <p key={index}>{item}</p>;
-			})}
+			<div id="listbar">
+				{inputList.map(todo => (
+					<>
+						<li key={todo}>
+							{todo}
+							<button
+								className="btn btn-danger"
+								id="delete"
+								onClick={() => delItem(inputList.id)}>
+								<i
+									className="fa fa-times"
+									aria-hidden="true"></i>
+							</button>
+						</li>
+					</>
+				))}
+				<div className="row">
+					<div className="col mx-auto">
+						<ul className="list-group">
+							{inputList.length == 0 ? (
+								<li className="list-group-item">
+									Add your task...
+								</li>
+							) : (
+								""
+							)}
+							<li
+								id="last"
+								className="list-group text-muted text-start counter">
+								{inputList.length} Items left
+							</li>
+						</ul>
+					</div>
+				</div>
+			</div>
 		</div>
 	);
 };
-export default Home;
+export default Todo;
