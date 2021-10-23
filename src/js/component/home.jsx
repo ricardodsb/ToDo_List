ed642b4235446c8a6ea827fa9a3e81a3c0d6c3de
ricardodsb <ricardojdasilva15@gmail.com>
2021-10-23 11:09:37 +0000

    trying to add APIs

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,17 +1,42 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 
 const Todo = () => {
 	const [inputTask, setInputTask] = useState("");
-	const [inputList, setInputList] = useState([]);
+	const [inputList, setInputList] = useState([
+		{ label: "Walk the dog", done: false }
+	]);
 
-	function delItem(id) {
-		let setInput = inputList.filter(inputTask => inputTask.id !== id);
+	useEffect(() => {
+		fetch("https://assets.breatheco.de/apis/fake/todos/user/ricardodsb", {
+			method: "GET",
+			body: JSON.stringify(inputList),
+			headers: {
+				"Content-Type": "application/json"
+			}
+		})
+			.then(resp => {
+				console.log(resp.ok); // Será true (verdad) si la respuesta es exitosa.
+				console.log(resp.status); // el código de estado = 200 o código = 400 etc.
+				return resp.json(); // (regresa una promesa) will try to parse the result as json as return a promise that you can .then for results
+			})
+			.then(data => {
+				//Aquí es donde debe comenzar tu código después de que finalice la búsqueda
+				console.log(data); //esto imprimirá en la consola el objeto exacto recibido del servidor
+			})
+			.catch(error => {
+				//manejo de errores
+				console.log(error);
+			});
+	}, []);
+
+	function delItem(index) {
+		let setInput = inputList.filter((inputTask, i) => i !== index);
 		setInputList(setInput);
 	}
 	function addItem(e) {
 		if (e.keyCode == 13) {
 			setInputTask(inputTask);
-			const list = inputList.concat(inputTask);
+			const list = inputList.concat({ label: inputTask, done: false });
 			setInputList(list);
 		}
 	}
@@ -26,14 +51,14 @@ const Todo = () => {
 				onKeyUp={addItem}
 			/>
 			<div id="listbar">
-				{inputList.map(todo => (
+				{inputList.map((todo, i) => (
 					<>
 						<li key={todo}>
-							{todo}
+							{todo.label}
 							<button
 								className="btn btn-danger"
 								id="delete"
-								onClick={() => delItem(inputList.id)}>
+								onClick={() => delItem(i)}>
 								<i
 									className="fa fa-times"
 									aria-hidden="true"></i>
